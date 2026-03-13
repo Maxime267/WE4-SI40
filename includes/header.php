97ff10102a734cfcd22f65c593b4c8521a7c2d4d
Jlcht <jl75ch@gmail.com>
2026-03-13 16:59:59 +0100

    First Commit

--- includes/header.php
+++ includes/header.php
@@ -3,28 +3,29 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Site de coaching personnel - Réalisez vos rêves et passez à l'action.">
     <title>Rendez vos Rêves Réalité - Coaching</title>
-    <link rel="stylesheet" href="/WE4-SI40/style.css">
+    <link rel="stylesheet" href="Style/style.css">
 </head>
 
 <body>
 
-<header class="site-header">
+    <header class="site-header">
 
-<div class="logo">
-    <h1>Coaching : Rendez-vos rêve réalité</h1>
-</div>
+        <div class="logo">
+            <h1>Coaching : Rendez-vos rêve réalité</h1>
+        </div>
 
-<nav class="menu">
+        <nav class="menu">
 
-<a href="/WE4-SI40/index.php">Accueil</a>
-<a href="/WE4-SI40/Qui_suis_je.php">Qui suis-je</a>
-<a href="/WE4-SI40/Rdv.php" class="btn-rdv">Prendre RDV</a>
+            <a href="index.php">Accueil</a>
+            <a href="Qui_suis_je.php">Qui suis-je</a>
+            <a href="Rdv.php" class="btn-rdv">Prendre RDV</a>
 
-</nav>
+        </nav>
 
-</header>
+    </header>
