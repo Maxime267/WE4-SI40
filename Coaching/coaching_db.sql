ed42b8c4c703b921ade632871b520b9cfb522c4b
Maxime <maxime.giraux@xuarig.fr>
2026-03-15 21:01:37 +0100

    dn change

--- Coaching/coaching_db.sql
+++ Coaching/coaching_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 15 mars 2026 à 16:55
+-- Généré le : dim. 15 mars 2026 à 21:00
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -29,11 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `appointments` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `date` date NOT NULL,
-  `time` time NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
   `message` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -45,8 +42,8 @@ CREATE TABLE `appointments` (
 --
 
 CREATE TABLE `block_dates` (
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
   `reason` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -55,7 +52,7 @@ CREATE TABLE `block_dates` (
 --
 
 INSERT INTO `block_dates` (`start_date`, `end_date`, `reason`) VALUES
-('2026-03-17', '2026-03-20', 'j\'ai pas envie lol');
+('2026-03-17 00:00:00', '2026-03-20 00:00:00', 'j\'ai pas envie lol');
 
 -- --------------------------------------------------------
 
@@ -71,6 +68,13 @@ CREATE TABLE `schedule_free_appointement` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Déchargement des données de la table `schedule_free_appointement`
+--
+
+INSERT INTO `schedule_free_appointement` (`id`, `day_of_week`, `start_time`, `end_time`) VALUES
+(1, 1, '11:00:00', '13:00:00');
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -78,8 +82,7 @@ CREATE TABLE `schedule_free_appointement` (
 -- Index pour la table `appointments`
 --
 ALTER TABLE `appointments`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `unique_slot` (`date`,`time`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `schedule_free_appointement`
@@ -101,7 +104,7 @@ ALTER TABLE `appointments`
 -- AUTO_INCREMENT pour la table `schedule_free_appointement`
 --
 ALTER TABLE `schedule_free_appointement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
