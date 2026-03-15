035ac32780cb2ad226e69324a7068afb04c0ce58
Maxime267 <maxime.giraux@xuarig.fr>
2026-03-15 14:39:43 +0100

    fix spam click on header

---
+++ test2.php
@@ -0,0 +1,55 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Réservation RDV</title>
+</head>
+<body>
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+<style>
+    .gray-day {
+        background: #eee;
+        color: #999;
+        pointer-events: none; /* empêche le clic */
+    }
+</style>
+
+<div id="calendar"></div>
+
+<script>
+
+    fetch("/api/blocked-dates")
+    .then(res => res.json())
+    .then(blockedDates => {
+
+
+    flatpickr("#calendar", {
+        inline: true,
+
+        locale: {
+            firstDayOfWeek: 1 // 0 = dimanche, 1 = lundi
+        },
+
+
+        disable: [
+            function(date) {
+                // Désactive samedi dimanche
+                return (date.getDay() === 0 || date.getDay() === 6);
+            }
+        ],
+    });
+</script>
+
+
+
+
+</body>
+</html>
