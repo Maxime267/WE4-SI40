7551a26639f8bdcb31fef311707e8a68eae7ca23
Maxime <maxime.giraux@xuarig.fr>
2026-03-15 19:26:26 +0100

    Amelioration style

--- test2.php
+++ test2.php
@@ -23,37 +23,26 @@
 </style>
 
 <div id="calendar"></div>
-
 <script>
-
-    fetch("/Coaching/api/blocked_dates.php")
-    .then(res => res.json())
-    .then(blockedDates => {
-
-
-    flatpickr("#calendar", {
-        inline: true,
-
-        locale: {
-            firstDayOfWeek: 1 // 0 = dimanche, 1 = lundi
-        },
-
-
-        disable: [
-            function(date) {
-                // Désactive samedi dimanche
-                return (date.getDay() === 0 || date.getDay() === 6);
-            },
-            ...blockedDates.map(date => new Date(date))
-        ],
-    });
-
-    });
-
+    fetch("Coaching/api/blocked_dates.php")
+        .then(res => res.json())
+        .then(data => {
+            // Accédez à data.blockedDates
+            flatpickr("#calendar", {
+                inline: true,
+                locale: {
+                    firstDayOfWeek: 1
+                },
+                disable: [
+                    function(date) {
+                        return (date.getDay() === 0 || date.getDay() === 6);
+                    },
+                    ...data.blockedDates.map(dateStr => new Date(dateStr))
+                ],
+            });
+        })
+        .catch(err => console.error("Erreur fetch:", err));
 </script>
-
-
-
 
 </body>
 </html>
